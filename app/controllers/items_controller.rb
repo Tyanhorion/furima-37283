d72b31a894f8be9966db02101ba500743af7439c
Tyanhorion <satiyo72@gmail.com>
2022-05-23 13:34:41 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,13 @@ class ItemsController < ApplicationController
   def index
   end
 
-  #def new
-     #@iteme = Iteme.new
-  #end
+  def new
+     @iteme = Iteme.new
+  end
+
+  private
+  def iteme_params
+    params.require(:iteme).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :buy
+  
+  has_one_attached :image
 end
