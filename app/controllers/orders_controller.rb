10878b3db4e3ea09dba91a563d2f8c46c3bf4479
Tyanhorion <satiyo72@gmail.com>
2022-06-09 11:20:07 +0900

    createに@itemを定義した

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,7 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(order_params)
     if @buy_address.valid?
       @buy_address.save
